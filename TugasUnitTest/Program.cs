7e57508bd7dcfa28c1a8e1eee8ea401537ca0787
ElanPratama <elan_pratama@students.amikom.ac.id>
2021-10-28 14:58:39 +0700

    Project 5

---
+++ TugasUnitTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
